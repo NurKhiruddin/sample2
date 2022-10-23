87c4b486800a9bf39f026acf7c5f146168c2dfa3
NurKhiruddin <115910461+NurKhiruddin@users.noreply.github.com>
2022-10-23 11:45:57 +0800

    Create 2iris_svm.py

---
+++ 2iris_svm.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+iris = sns.load_dataset('iris') # returns a pandas dataframe
+import pandas as pd
+
+
+X_iris = iris.drop('species', axis=1) 
+y_iris = iris['species']
+
+
+xtrain, xtest, ytrain, ytest = train_test_split(X_iris, y_iris, random_state = 0)
+clf = SVC(kernel='rbf', C=1).fit(xtrain, ytrain)
+st.write('Iris dataset')
+st.write('Accuracy of RBF SVC classifier on training set: {:.2f}'
+     .format(clf.score(xtrain, ytrain)))
+st.write('Accuracy of RBF SVC classifier on test set: {:.2f}'
+     .format(clf.score(xtest, ytest)))
