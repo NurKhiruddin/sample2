67fb76ed1a9125646157cf1da7ca32c8eba64ea6
NurKhiruddin <115910461+NurKhiruddin@users.noreply.github.com>
2022-10-23 11:25:11 +0800

    Create iris_svm.py

---
+++ iris_svm.py
@@ -0,0 +1,22 @@
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+#from sklearn.datasets import load_iris
+from sklearn.metrics import classification_report
+ 
+alist = [1, 2, 3, 4, 5]   # Define a python list. It looks like an np array
+narray = np.array([1, 2, 3, 4]) # Define a numpy array
+ 
+print(narray + narray)
+print(alist + alist)
+
+xtrain, xtest, ytrain, ytest = train_test_split(X_iris, y_iris, random_state = 0)
+
+clf = SVC(kernel='rbf', C=1).fit(xtrain, ytrain)
+print('Iris dataset')
+print('Accuracy of RBF SVC classifier on training set: {:.2f}'
+     .format(clf.score(xtrain, ytrain)))
+print('Accuracy of RBF SVC classifier on test set: {:.2f}'
+     .format(clf.score(xtest, ytest)))
