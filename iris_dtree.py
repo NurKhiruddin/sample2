34f4c2a87f4e7f682e33931198cec591fedd0e3e
NurKhiruddin <115910461+NurKhiruddin@users.noreply.github.com>
2022-10-23 12:03:08 +0800

    Create iris_dtree.py

---
+++ iris_dtree.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+from sklearn.tree import plot_tree
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+iris = sns.load_dataset('iris')
+
+X_iris = iris.drop('species', axis=1)  
+y_iris = iris['species']
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(X_iris, y_iris,random_state=1)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(Xtrain, ytrain)
+
+
+
+clf.fit(Xtrain, ytrain) 
+st.write((tree.plot_tree(clf.fit(Xtrain, ytrain) ))
+
+st.header("Mean accurary is")
+st.write(clf.score(Xtest, ytest))
