1720c11d33355fa914b63c9f1fe66ea748921275
NurKhiruddin <115910461+NurKhiruddin@users.noreply.github.com>
2022-10-23 11:29:31 +0800

    Update exercise_mall.py

--- exercise_mall.py
+++ exercise_mall.py
@@ -1,7 +1,10 @@
-import seaborn as sns
+import streamlit as st
+import numpy as np
 import pandas as pd
+import seaborn as sns
+import sklearn as sk
 import matplotlib.pyplot as plt
-mc = pd.read_csv('/content/mall_customer.csv')
+mc = pd.read_csv('mall_customer.csv')
 
 mc.head()
 
@@ -11,9 +14,8 @@ mc.describe()
 
 mc.info()
 
-x_mc = mc.drop(['Genre','CustomerID'], axis=1)  
+x_mc = mc.drop(['Genre','CustomerID'], axis=1) 
 x_mc
-
 y_mc = mc['Genre']
 y_mc
 
@@ -21,33 +23,25 @@ from sklearn.model_selection import train_test_split
 Xtrain, Xtest, ytrain, ytest = train_test_split(x_mc, y_mc)
 Xtrain.head()
 
-Xtrain.shape #75%
-
-Xtest.shape #25%
-
 from sklearn.naive_bayes import GaussianNB
 model = GaussianNB()                       
 model.fit(Xtrain, ytrain)                 
 y_model = model.predict(Xtest)
 
 from sklearn.metrics import accuracy_score
-accuracy_score(ytest, y_model) #accuracy is low
+a = accuracy_score(ytest, y_model)
 
-from sklearn.metrics import classification_report
+st.write(a)
 
-print(classification_report(ytest, y_model))
-
-# Confusion Matrix
-from sklearn.metrics import confusion_matrix 
+from sklearn.metrics import confusion_matrix
 confusion_matrix(ytest, y_model)
 
-#Confusion Matrix
 import matplotlib.pyplot as plt
 from sklearn import metrics
 import numpy as np
 confusion_matrix = metrics.confusion_matrix(ytest, y_model)
 
-print(confusion_matrix)
+st.write(confusion_matrix)
 
 cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix,display_labels=np.unique(y_mc))
 
@@ -55,4 +49,4 @@ cm_display.plot()
 plt.show()
 
 from sklearn.metrics import classification_report
-print(classification_report(ytest, y_model))
+st.write(classification_report(ytest, y_model))
